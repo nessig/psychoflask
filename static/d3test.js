b64810646fb65322878299cc783d9ec9210aa166
Nolan Essigmann <nessig@users.noreply.github.com>
2016-02-12 22:07:11 -0500

    cleaned up backups

--- static/d3test.js
+++ static/d3test.js
@@ -1,76 +1,20 @@
-var circleData = [{
-    "cx": 20,
-    "cy": 20,
-    "radius": 20,
-    "color": "green"
-}, {
-    "cx": 70,
-    "cy": 70,
-    "radius": 20,
-    "color": "purple"
-}];
+var svg = d3.select("body").append("svg")
+    .attr("width", 500)
+    .attr("height", 500);
+
+var group = svg.append("g")
+    .attr("transform", "translate(100,100)");
+
+var r = 100;
+var p = Math.PI * 2;
 
 
-var rectangleData = [{
-    "rx": 110,
-    "ry": 110,
-    "height": 30,
-    "width": 30,
-    "color": "blue"
-}, {
-    "rx": 160,
-    "ry": 160,
-    "height": 30,
-    "width": 30,
-    "color": "red"
-}];
 
-var svgContainer = d3.select("body").append("svg")
-    .attr("width", 200)
-    .attr("height", 200);
+var arc = d3.svg.arc()
+    .innerRadius(r - 20)
+    .outerRadius(r)
+    .startAngle(0)
+    .endAngle(p - 1);
 
-var circleGroup = svgContainer.append("g")
-    .attr("transform", "translate(80,0)");
-
-
-var circles = circleGroup.selectAll("circle")
-    .data(circleData)
-    .enter()
-    .append("circle");
-
-var circleAttributes = circles
-    .attr("cx", function(d) {
-        return d.cx;
-    })
-    .attr("cy", function(d) {
-        return d.cy;
-    })
-    .attr("r", function(d) {
-        return d.radius;
-    })
-    .style("fill", function(d) {
-        return d.color;
-    });
-
-var rectangles = svgContainer.selectAll("rect")
-    .data(rectangleData)
-    .enter()
-    .append("rect");
-
-var rectangleAttributes = rectangles
-    .attr("x", function(d) {
-        return d.rx;
-    })
-    .attr("y", function(d) {
-        return d.ry;
-    })
-    .attr("height", function(d) {
-        return d.height;
-    })
-    .attr("width", function(d) {
-        return d.width;
-    })
-    .style("fill", function(d) {
-        return d.color;
-    });
-
+group.append("path")
+    .attr("d", arc)
